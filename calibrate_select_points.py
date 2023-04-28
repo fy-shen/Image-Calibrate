555d4aa62273aa4f0eef91a0ca726061f1b9086f
sfysfy <sfy849015575@outlook.com>
2023-04-28 15:14:33 +0800

    Add data

--- calibrate_select_points.py
+++ calibrate_select_points.py
@@ -24,6 +24,7 @@ class App:
         self.camera_img = cv2.imread(camera_img_path)
         self.camera_kpts = None if camera_kpts_path is None else np.load(camera_kpts_path)['kpts']
         self.camera_kpts_select = self.read_select_kpts(camera_select_kpts_path)
+        self.camera_kpts_select_raw = None
         self.camera_show = None
 
         self.pattern_window = 'pattern'
@@ -31,6 +32,7 @@ class App:
         self.pattern_img = cv2.imread(pattern_img_path)
         self.pattern_kpts = None if pattern_kpts_path is None else np.load(pattern_kpts_path)['kpts']
         self.pattern_kpts_select = self.read_select_kpts(pattern_select_kpts_path)
+        self.pattern_kpts_select_raw = None
         self.pattern_show = None
 
     @staticmethod
@@ -57,6 +59,29 @@ class App:
             return True
         else:
             return False
+
+    def random_select_kpts(self):
+        n = len(self.camera_kpts_select_raw)
+        if self.random_select >= n or self.random_select < 5:
+            print('不进入随机取点')
+            self.random_select = -1
+        else:
+            camera_rand_kpts = []
+            pattern_rand_kpts = []
+            rand_idx = np.random.randint(0, n, size=self.random_select)
+            for idx in rand_idx:
+                camera_rand_kpts.append(self.camera_kpts_select_raw[idx])
+                pattern_rand_kpts.append(self.pattern_kpts_select_raw[idx])
+            self.camera_kpts_select = camera_rand_kpts
+            self.pattern_kpts_select = pattern_rand_kpts
+            self.camera_show = show_img_with_kpts(
+                self.camera_img, self.camera_kpts_select, self.camera_window, kpt_size=5, font_size=1
+            )
+            self.pattern_show = show_img_with_kpts(
+                self.pattern_img, self.pattern_kpts_select, self.pattern_window, kpt_size=10, font_size=2
+            )
+            print(f'在手动选择的{n}个点中随机选取{self.random_select}个点')
+            self.calibrate()
 
     def camera_mouse(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -86,38 +111,50 @@ class App:
 
     def calibrate(self):
         _, name, _ = splitfn(self.camera_img_path)
-        cv2.imwrite(os.path.join(self.save_root, f"{name}_camera_select_kpts.jpg"), self.camera_show)
-        cv2.imwrite(os.path.join(self.save_root, f"{name}_pattern_select_kpts.jpg"), self.pattern_show)
 
         camera_kpts = np.array(self.camera_kpts_select, dtype=np.float32)
-        np.savez(os.path.join(self.save_root, f"{name}_camera_select_kpts.npz"), kpts=camera_kpts)
-        pattern_kpts = np.array(self.pattern_kpts_select, dtype=np.float32)
-        np.savez(os.path.join(self.save_root, f"{name}_pattern_select_kpts.npz"), kpts=pattern_kpts)
-        pattern_kpts = np.concatenate((pattern_kpts, np.zeros((len(pattern_kpts), 1))), axis=1).astype(np.float32)
+        pattern_kpts2 = np.array(self.pattern_kpts_select, dtype=np.float32)
+        pattern_kpts = np.concatenate((pattern_kpts2, np.zeros((len(pattern_kpts2), 1))), axis=1).astype(np.float32)
 
+        # 畸变矫正
         camera_matrix, dist_coefs, _rvecs, _tvecs, newcameramtx, dst, dst_full = calibrate(
             self.camera_img, camera_kpts, pattern_kpts, self.fisheye
         )
+        # 单应性变换
         hom, dst_hom = find_hom(dst_full, self.pattern_img, pattern_kpts, _rvecs[0], _tvecs[0], newcameramtx)
 
+        # show
         cv2.namedWindow("dst", cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
         cv2.namedWindow("dst_full", cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
         cv2.namedWindow("dst_hom", cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
         cv2.imshow("dst", dst)
         cv2.imshow("dst_full", dst_full)
         cv2.imshow("dst_hom", dst_hom)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        key = cv2.waitKey(0)
 
-        cv2.imwrite(os.path.join(self.save_root, f"{name}_result.jpg"), dst)
-        cv2.imwrite(os.path.join(self.save_root, f"{name}_result_full.jpg"), dst_full)
-        cv2.imwrite(os.path.join(self.save_root, f"{name}_hom.jpg"), dst_hom)
-        np.savez(os.path.join(self.save_root, f"{name}_calibrate_parm.npz"),
-                 camera_matrix=camera_matrix,
-                 dist_coefs=dist_coefs,
-                 _rvecs=_rvecs,
-                 _tvecs=_tvecs)
-        np.savez(os.path.join(self.save_root, f"{name}_hom.npz"), hom=hom)
+        if key == ord('r') and self.random_select != -1:
+            self.random_select_kpts()
+        else:
+            # save
+            cv2.imwrite(os.path.join(self.save_root, f"{name}_camera_select_kpts.jpg"), self.camera_show)
+            cv2.imwrite(os.path.join(self.save_root, f"{name}_pattern_select_kpts.jpg"), self.pattern_show)
+            np.savez(os.path.join(self.save_root, f"{name}_camera_select_kpts.npz"), kpts=camera_kpts)
+            np.savez(os.path.join(self.save_root, f"{name}_pattern_select_kpts.npz"), kpts=pattern_kpts2)
+            cv2.imwrite(os.path.join(self.save_root, f"{name}_result.jpg"), dst)
+            cv2.imwrite(os.path.join(self.save_root, f"{name}_result_full.jpg"), dst_full)
+            cv2.imwrite(os.path.join(self.save_root, f"{name}_hom.jpg"), dst_hom)
+            np.savez(os.path.join(self.save_root, f"{name}_calibrate_parm.npz"),
+                     camera_matrix=camera_matrix,
+                     dist_coefs=dist_coefs,
+                     _rvecs=_rvecs,
+                     _tvecs=_tvecs)
+            np.savez(os.path.join(self.save_root, f"{name}_hom.npz"), hom=hom)
+            if self.random_select != -1:
+                camera_kpts_raw = np.array(self.camera_kpts_select_raw, dtype=np.float32)
+                pattern_kpts_raw = np.array(self.pattern_kpts_select_raw, dtype=np.float32)
+                np.savez(os.path.join(self.save_root, f"{name}_camera_select_kpts_raw.npz"), kpts=camera_kpts_raw)
+                np.savez(os.path.join(self.save_root, f"{name}_pattern_select_kpts_raw.npz"), kpts=pattern_kpts_raw)
+            cv2.destroyAllWindows()
 
     def run(self):
         cv2.namedWindow(self.camera_window, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
@@ -139,7 +176,8 @@ class App:
                 break
             elif key == ord('s'):
                 if self.check_select_kpts():
-                    cv2.destroyAllWindows()
+                    self.camera_kpts_select_raw = self.camera_kpts_select
+                    self.pattern_kpts_select_raw = self.pattern_kpts_select
                     print("开始矫正")
                     self.calibrate()
                     break
@@ -149,13 +187,13 @@ class App:
 
 if __name__ == "__main__":
     calibration = App(
-        camera_img_path='data/nike/left_side_2.jpg',
+        camera_img_path='data/nike/left_side_1.jpg',
         pattern_img_path='data/pattern/court.jpg',
         camera_kpts_path=None,
         pattern_kpts_path='data/pattern/court_kpts.npz',
-        camera_select_kpts_path='data/nike/left_side_2_camera_select_kpts.npz',
-        pattern_select_kpts_path='data/nike/left_side_2_pattern_select_kpts.npz',
+        camera_select_kpts_path='data/nike/left_side_1_camera_select_kpts.npz',
+        pattern_select_kpts_path='data/nike/left_side_1_pattern_select_kpts.npz',
         fisheye=False,
-        random_select=-1
+        random_select=30
     )
     calibration.run()
